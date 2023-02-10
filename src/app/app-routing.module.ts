e278380f72d4ed7ba2f8f020d0261135fe12b83b
mayank@algogenix.com <mayank@algogenix.com>
2023-02-11 00:17:10 +0530

    .jjj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'registrationPage',component:RegistrationPageComponent,}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login-page';
+  submitForm(data: any) {
+    console.log(data);
 }
+title = 'login-page';
+currentApplicationVersion = environment.appVersion;
+constructor(
+) {
+}
+}
+
+
